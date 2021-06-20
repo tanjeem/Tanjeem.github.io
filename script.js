c4ec469072ed9ed26d94865514d76320922f6c19
tanjeem <tanjeem.adeeb@gmail.com>
2021-06-20 23:21:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/*function jump(h){
+    var top = document.getElementById(h).offsetTop;
+    window.scrollTo(0, top);
+} */
+
+document.querySelector('.home').scrollIntoView({ 
+    behavior: 'smooth' });
+
+
+document.querySelector('.about').scrollIntoView({ 
+        behavior: 'smooth' });
+document.querySelector('.skills').scrollIntoView({ 
+        behavior: 'smooth' });
+document.querySelector('.contact').scrollIntoView({ 
+        behavior: 'smooth' });
+
